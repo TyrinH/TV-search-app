71e468ee425d498f45d90aae40fd26a2274c29e2
Tyrin Holloway <tychnk@gmail.com>
2021-09-17 19:54:50 -0500

    created ability to capture form input value

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const searchForm = document.querySelector('#searchForm')
 
+searchForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    console.dir(searchForm.query.value)
+})
 const getTvShows = async () => {
     try {
         const res = await axios.get('https://api.tvmaze.com/search/shows?q=girls')
-        console.log(res.data)
+        console.log(res.data[0].show.name)
 
     } catch (e){
         console.log(e)
